72228734fe52ac01ae79d628dd1bb6881487486a
devansh <devansh@umich.edu>
2023-09-05 17:49:17 -0400

    clang-format

--- src/vicon_px4_bridge.cpp
+++ src/vicon_px4_bridge.cpp
@@ -19,10 +19,10 @@ public:
     this->get_parameter("px4_name", px4_name);
     this->get_parameter("vicon_name", vicon_name);
     std::string vicon_sub_name = "vicon/" + vicon_name + "/" + vicon_name;
-    
+
     std::string timesync_sub_name = px4_name + "/fmu/out/timesync";
     std::string px4_pub_name = px4_name + "/fmu/in/vehicle_visual_odometry";
-    
+
     RCLCPP_INFO(this->get_logger(), "vicon_sub_name: %s \n",
                 vicon_sub_name.c_str());
     RCLCPP_INFO(this->get_logger(), "timesync_sub_name: %s \n",
@@ -30,16 +30,15 @@ public:
     RCLCPP_INFO(this->get_logger(), "px4_pub_name: %s \n",
                 px4_pub_name.c_str());
     RCLCPP_INFO(this->get_logger(), "Using Vicon -> Visual Odometry callback!");
-    
-    
-    position_sub_ = this->create_subscription<geometry_msgs::msg::TransformStamped>(
-        vicon_sub_name, 10,
-	std::bind(&ViconPX4Bridge::position_callback, this, _1));
 
-    odometry_pub_ =
-        this->create_publisher<px4_msgs::msg::VehicleOdometry>(
-            px4_pub_name, 10);
-    
+    position_sub_ =
+        this->create_subscription<geometry_msgs::msg::TransformStamped>(
+            vicon_sub_name, 10,
+            std::bind(&ViconPX4Bridge::position_callback, this, _1));
+
+    odometry_pub_ = this->create_publisher<px4_msgs::msg::VehicleOdometry>(
+        px4_pub_name, 10);
+
     timesync_sub_ = this->create_subscription<px4_msgs::msg::TimesyncStatus>(
         timesync_sub_name, 10,
         [this](const px4_msgs::msg::TimesyncStatus::UniquePtr msg) {
@@ -52,10 +51,10 @@ public:
 private:
   std::string px4_name;
 
-  rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr
-      odometry_pub_;
+  rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr odometry_pub_;
 
-  rclcpp::Subscription<geometry_msgs::msg::TransformStamped>::SharedPtr position_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::TransformStamped>::SharedPtr
+      position_sub_;
   rclcpp::Subscription<px4_msgs::msg::TimesyncStatus>::SharedPtr timesync_sub_;
   std::atomic<uint64_t> px4_timestamp_;
   std::atomic<uint64_t> px4_server_timestamp_;
@@ -67,7 +66,8 @@ private:
     return px4_timestamp_.load() + delta;
   }
 
-  void position_callback(const geometry_msgs::msg::TransformStamped &vicon_msg) {
+  void
+  position_callback(const geometry_msgs::msg::TransformStamped &vicon_msg) {
     auto message = px4_msgs::msg::VehicleOdometry();
 
     // timestamps
@@ -78,7 +78,7 @@ private:
     // pose frame
     message.pose_frame = px4_msgs::msg::VehicleOdometry::POSE_FRAME_NED;
 
-    //position
+    // position
     message.position[0] = vicon_msg.transform.translation.y;
     message.position[1] = vicon_msg.transform.translation.x;
     message.position[2] = -vicon_msg.transform.translation.z;
@@ -89,40 +89,36 @@ private:
     auto yaw = tf2::getYaw(tf2_vicon);
     auto yaw_ned = -yaw + 1.57;
     Eigen::Quaternion<double> out(cos(yaw_ned / 2.0), 0, 0, sin(yaw_ned / 2.0));
-    
+
     message.q[0] = out.w();
     message.q[1] = out.x();
     message.q[2] = out.y();
     message.q[3] = out.z();
-    
-    // velocity frame 
+
+    // velocity frame
     message.velocity_frame =
         px4_msgs::msg::VehicleOdometry::VELOCITY_FRAME_UNKNOWN;
 
+    for (size_t i = 0; i < 3; i++) {
 
-    for (size_t i=0; i<3; i++) {
+      // velocity
+      message.velocity[i] = NAN;
 
-    // velocity
-    message.velocity[i] = NAN;
-
-    // angular velocity
-    message.angular_velocity[i] = NAN;
+      // angular velocity
+      message.angular_velocity[i] = NAN;
     }
-
 
     // position covariance
-    for (size_t i=0; i<3; i++) {
-	    message.position_variance[i] = 0.01;
-	    message.orientation_variance[i] = 0.0523; // approx 3 degrees
-	    message.velocity_variance[i] = NAN;
+    for (size_t i = 0; i < 3; i++) {
+      message.position_variance[i] = 0.01;
+      message.orientation_variance[i] = 0.0523; // approx 3 degrees
+      message.velocity_variance[i] = NAN;
     }
-    
+
     message.reset_counter = 0;
 
     odometry_pub_->publish(message);
-
   }
-
 };
 
 int main(int argc, char *argv[]) {
